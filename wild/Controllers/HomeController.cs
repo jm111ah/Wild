9925666a9c6fdf81b26f7342cf50f4c21f820001
tacoyaggi93 <tacoyaggi93@gmail.com>
2021-04-26 10:40:06 +0900

    [정성균] ㅇ

--- wild/Controllers/HomeController.cs
+++ wild/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +12,7 @@ namespace wild.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
@@ -17,6 +21,31 @@ namespace wild.Controllers
         [HttpPost]
         public IActionResult Index(User user)
         {
+            string con = "Data Source=(local);Initial Catalog=pubs;Integrated Security=SSPI;";
+            string query = "select * from t_member where siteid = H15";
+            DataSet ds = null;
+            SqlConnection sql = null;
+            try
+            {
+                sql = new SqlConnection(con);
+                sql.Open();
+                SqlCommand cmd = new SqlCommand(query,sql);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                if (sql != null)
+                {
+                    sql.Close();
+                }
+            }
+            
+
             return View();
         }
     }
